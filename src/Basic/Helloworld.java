9da0ca3ae5869049f50cd8f059f92897e6f77b99
Bharathigm <bharathigm18gmail.com>
2021-02-27 16:41:26 +0530

    init

---
+++ src/Basic/Helloworld.java
@@ -0,0 +1,15 @@
+
+package Basic;
+
+
+public class Helloworld {
+
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("welcome: ");
+
+	}
+
+}
